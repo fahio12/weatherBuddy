163aac5802add9255627ebc65bf7b4cf09eda064
Fabio Hernandez <44535804+fahio12@users.noreply.github.com>
2020-06-01 04:00:48 -0400

    clusters

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const weather = require('weather-js');
 
-
-weather.find({search:'Miami,Fl',degreeType:'F'},function (err,result){
+weather.find({search:'Miami Dade',degreeType:'F'},function (err,result){
   if(err) console.log(err)
   console.log(result)
 })
